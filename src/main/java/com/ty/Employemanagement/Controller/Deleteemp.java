c38d4d34818d790d102cafc68d8d76cd72207c84
Prabir Rout <prabirrout32@gmail.com>
2025-05-05 15:01:42 +0530

    Update Deleteemp.java

--- src/main/java/com/ty/Employemanagement/Controller/Deleteemp.java
+++ src/main/java/com/ty/Employemanagement/Controller/Deleteemp.java
@@ -1,18 +1,13 @@
 package com.ty.Employemanagement.Controller;
 
-
-
 import java.io.IOException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.ty.Employemanagement.service.EmpService;
-
 
 @WebServlet(value = "/delete")
 public class Deleteemp extends HttpServlet {
